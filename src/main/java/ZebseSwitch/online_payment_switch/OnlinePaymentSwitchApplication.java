0bcbb02f17431d5409aa61c2190c51d3f4a1e189
Kebede Assefa <zebserra2121@gmail.com>
2026-06-17 11:37:53 +0300

    Initial commit - Spring Boot payment switch foundation

---
+++ src/main/java/ZebseSwitch/online_payment_switch/OnlinePaymentSwitchApplication.java
@@ -0,0 +1,13 @@
+package ZebseSwitch.online_payment_switch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinePaymentSwitchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinePaymentSwitchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ZebseSwitch/online_payment_switch/OnlinePaymentSwitchApplicationTests.java
@@ -0,0 +1,13 @@
+package ZebseSwitch.online_payment_switch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinePaymentSwitchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
